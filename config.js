a9e6bcba65861f9fa637d2e7c92d34b4951410c0
thabtom <tsehayegh@gmail.com>
2018-03-06 18:54:20 -0500

    added more process env variables at config.js

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
+						process.env.MONGODB_URI ||
+						process.env.MONGOHQ_URL ||
+						process.env.MONGOLAB_URI ||
                       'mongodb://localhost/blogs-app';
+
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                       'mongodb://localhost/test-blogs-app';
 exports.PORT = process.env.PORT || 8080;
